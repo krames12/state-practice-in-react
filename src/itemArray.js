cd14ea16ec5f16efd004ebf12db9928db37c8ea8
Nick Kramer <krames12@gmail.com>
2018-05-23 11:28:10 -0400

    added a category array to each item

--- src/itemArray.js
+++ src/itemArray.js
@@ -2,42 +2,74 @@ const itemArray = [
     {
         "id": 1,
         "name": "Can",
-        "price": 1.42
+        "price": 1.42,
+        "category": [
+            "Misc"
+        ],
     },
     {
         "id": 2,
         "name": "Latte",
-        "price": 5.43
+        "price": 5.43,
+        "category": [
+            "Drink"
+        ],
     },
     {
         "id": 3,
         "name": "Cup O Noodle",
-        "price": 0.87
+        "price": 0.87,
+        "category": [
+            "Food"
+        ],
     },
     {
         "id": 4,
         "name": "Sweet Tarts",
-        "price": 1.39
+        "price": 1.39,
+        "category": [
+            "Food"
+        ],
     },
     {
         "id": 5,
         "name": "Giant Sweet Tarts",
-        "price": 3.00
+        "price": 3.00,
+        "category": [
+            "Food"
+        ],
     },
     {
         "id": 6,
         "name": "Red Velvet Cake Slice",
-        "price": 2.75
+        "price": 2.75,
+        "category": [
+            "Food"
+        ],
     },
     {
         "id": 7,
         "name": "Oatmeal",
-        "price": 1.44
+        "price": 1.44,
+        "category": [
+            "Food"
+        ],
     },
     {
         "id": 8,
         "name": "Pancakes",
-        "price": 6.74
+        "price": 6.74,
+        "category": [
+            "Food"
+        ],
+    },
+    {
+        "id": 9,
+        "name": "Coffee",
+        "price": 3.54,
+        "category": [
+            "Drink"
+        ],
     },
 ];
 
