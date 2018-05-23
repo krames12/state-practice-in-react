3ca3d22b2fbcbefb389357da28da0017694b1f8b
Nick Kramer <krames12@gmail.com>
2018-05-23 11:08:41 -0400

    added in subtotal / tax box above pay button

--- src/CheckoutTotals.js
+++ src/CheckoutTotals.js
@@ -7,7 +7,21 @@ class CheckoutTotals extends Component {
             <div id="checkout-totals-container">
                 <CheckoutConsumer.Consumer>
                     { ({total}) => (
-                        <button className="pay-total-button">Pay Total: {total}</button>
+                        <React.Fragment>
+                            <div className="totals-container">
+                                <p>
+                                    <span className="total-title-text">Subtotal</span>
+                                    <span className="total-amount-text">${total}</span>
+                                </p>
+                                <p>
+                                    <span className="total-title-text">Tax</span>
+                                    <span className="total-amount-text">$0</span>
+                                </p>
+                            </div>
+                            <div className="pay-button-container">
+                                <button className="pay-total-button">Pay Total: ${total}</button>
+                            </div>
+                        </React.Fragment>
                     ) }
                 </CheckoutConsumer.Consumer>
             </div>
